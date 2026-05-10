0671edfa8af9b507aad29831f2e65df686b5016e
Nick Konstantinou <nkonstantinou@terracom.gr>
2026-05-10 10:30:20 +0300

    Initial Commit

---
+++ Nimbus/NimbusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusApp.swift
+//  Nimbus
+//
+//  Created by Nick Konstantinou on 10/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NimbusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NimbusTests/NimbusTests.swift
@@ -0,0 +1,19 @@
+//
+//  NimbusTests.swift
+//  NimbusTests
+//
+//  Created by Nick Konstantinou on 10/5/26.
+//
+
+import Testing
+@testable import Nimbus
+
+struct NimbusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
